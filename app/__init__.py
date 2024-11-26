a674200ec68a83e5bd1cbdf2d3af0f2d0febc0ae
Robel231 <robelshemeles4@gmail.com>
2024-11-26 10:04:59 -0800

    d

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
-from flask import Flask
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from .models import Portfolio, BlogPost, ContactSubmission, Photo, Video, TextContent
-from . import db
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mihret.db'
-
-    # Initialize Flask-Admin
-    admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
-
-    # Add models to Flask-Admin
-    admin.add_view(ModelView(Portfolio, db.session))
-    admin.add_view(ModelView(BlogPost, db.session))
-    admin.add_view(ModelView(ContactSubmission, db.session))
-    admin.add_view(ModelView(Photo, db.session))
-    admin.add_view(ModelView(Video, db.session))
-    admin.add_view(ModelView(TextContent, db.session))
-
-    # Initialize other extensions
-    db.init_app(app)
 
 # Initialize extensions
 db = SQLAlchemy()
